bdb4a7d95ca9a7b40b2441dc75e55b993423bff7
jimmy-glitch <76778937+AHMED-GAMAL-AG@users.noreply.github.com>
2023-01-11 08:50:50 +0200

    04. طلبات الاستمارات Form Requests

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -2,14 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\storeComment;
 use App\Models\Comment;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
 class CommentsController extends Controller
 {
-    public function store(Post $post)
+    public function store(storeComment $request ,Post $post)
     {
+        $request->rules; // php artisan make:request storeComment to use the rules() method in it to validate the user input on comment fides
+
         Comment::create([
             'name' => request('name'),
             'body' => request('body'),
