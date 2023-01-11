a13341a530e4db905ee574f2f7e7b2fd3c920da9
jimmy-glitch <76778937+AHMED-GAMAL-AG@users.noreply.github.com>
2023-01-11 13:18:48 +0200

    05. اللغة العربية وتعدد اللغات في التطبيق

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB; // i used DB to access to use query builder
 use Carbon\Carbon; // i used Carbon package to handle time it comes with laravel you can install it in any project
+use Illuminate\Support\Facades\App;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -32,4 +34,14 @@ Route::get('/', function () {
 // route name , controller name
 Route::resource('posts', PostsController::class);
 
-Route::post('/posts/{post}/comments' , [CommentsController::class , 'store']);
+Route::post('/posts/{post}/comments', [CommentsController::class, 'store']);
+
+Route::get('welcome', function () {
+    App::setLocale('ar');
+    echo (__('messages.welcome'));
+});
+
+Route::get('/signup/{lang}', function ($lang) {
+    App::setLocale($lang);
+    return view('signup');
+});
