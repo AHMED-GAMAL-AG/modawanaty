3314842a2f3fed197db4f4e2bb8775709c7a0640
jimmy-glitch <76778937+AHMED-GAMAL-AG@users.noreply.github.com>
2023-01-12 09:52:03 +0200

    adding subscribe to a  component

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -30,26 +30,7 @@
             </div><!-- /.blog-main -->
 
             <aside class="col-md-4 blog-sidebar">
-                <div class="p-4 mb-3 bg-light rounded">
-                    <h4>مبادئ التسويق الإلكتروني</h4>
-                    <p>يكمنك الان شراء نسختك من كتاب مبادئ التسويق الإلكتروني بتخفيض قدره %30 أدخل بريدك الإلكتروني
-                        لتصلك رسالة بالتفاصيل</p>
-                    <form action="/mail" method="POST">
-                        @csrf
-                        <div class="form-group">
-                            <input type="email" name="email" id="email" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-primary">ارسال</button>
-                        </div>
-                    </form>
-                </div>
-                @error('mail')
-                    <div class="alert alert-danger alert-dismissible fade show">{{ $message }}
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
-                                aria-hidden="true">&times;</span></button>
-                    </div>
-                @enderror
+                <x-subscribe />
             </aside><!-- /.blog-sidebar -->
 
         </div><!-- /.row -->
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,56 +1,25 @@
-@section('title' , $post->title)
+@section('title', $post->title)
 
 <x-layout>
     {{-- you should define the $post and $comments variable in the constructor in app/view/components/post.php --}}
     {{-- you should pass the class as {{$attributes}} in the <x-post> component --}}
-        <x-post :post="$post" class="blog-post m-3" />
+    <x-post :post="$post" class="blog-post m-3" />
 
-        <x-comment :comments="$comments" />
+    <x-comment :comments="$comments" />
 
-        <a class="btn btn-primary" href="/posts/{{$post->id}}/edit">تعديل المقالة</a>
-        <h3>أضف تعليقًا</h3>
-        <x-createComment :post="$post" />
+    <a class="btn btn-primary" href="/posts/{{ $post->id }}/edit">تعديل المقالة</a>
+    <h3>أضف تعليقًا</h3>
+    <x-createComment :post="$post" />
 
 
-        </div><!-- /.blog-main -->
+    </div><!-- /.blog-main -->
 
-        <aside class="col-md-4 blog-sidebar">
-            <div class="p-4 mb-3 bg-light rounded">
-                <h4 class="font-italic">About</h4>
-                <p class="mb-0">Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur
-                    purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-            </div>
+    <aside class="col-md-4 blog-sidebar">
+        <x-subscribe />
+    </aside><!-- /.blog-sidebar -->
 
-            <div class="p-4">
-                <h4 class="font-italic">Archives</h4>
-                <ol class="list-unstyled mb-0">
-                    <li><a href="#">March 2014</a></li>
-                    <li><a href="#">February 2014</a></li>
-                    <li><a href="#">January 2014</a></li>
-                    <li><a href="#">December 2013</a></li>
-                    <li><a href="#">November 2013</a></li>
-                    <li><a href="#">October 2013</a></li>
-                    <li><a href="#">September 2013</a></li>
-                    <li><a href="#">August 2013</a></li>
-                    <li><a href="#">July 2013</a></li>
-                    <li><a href="#">June 2013</a></li>
-                    <li><a href="#">May 2013</a></li>
-                    <li><a href="#">April 2013</a></li>
-                </ol>
-            </div>
+    </div><!-- /.row -->
 
-            <div class="p-4">
-                <h4 class="font-italic">Elsewhere</h4>
-                <ol class="list-unstyled">
-                    <li><a href="#">GitHub</a></li>
-                    <li><a href="#">Twitter</a></li>
-                    <li><a href="#">Facebook</a></li>
-                </ol>
-            </div>
-        </aside><!-- /.blog-sidebar -->
-
-        </div><!-- /.row -->
-
-        </main><!-- /.container -->
-        </div>
+    </main><!-- /.container -->
+    </div>
 </x-layout>
